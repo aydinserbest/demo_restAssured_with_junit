a6c4a5616d990ad500e72a0b22c648932cb42a39
aydinSerbest <aydinserbest34@gmail.com>
2024-02-12 20:54:55 +0100

    Initial commit

---
+++ src/test/java/acceptancetests/JSONPathAssertionExercise.java
@@ -0,0 +1,26 @@
+package acceptancetests;
+
+import io.restassured.RestAssured;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.serenitybdd.rest.SerenityRest;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static net.serenitybdd.rest.SerenityRest.given;
+import static org.hamcrest.Matchers.equalTo;
+
+@RunWith(SerenityRunner.class)
+public class JSONPathAssertionExercise {
+    @Before
+    public void prepare_rest_config() {
+        RestAssured.baseURI = "http://localhost:9000/api/";
+    }
+
+    @Test
+    public void find_a_simple_field_value() {
+        given().pathParam("symbol", "aapl")
+                .when().get("stock/{symbol}/company")
+                .then().body("industry", equalTo("Telecommunications Equipment"));
+    }
+}
